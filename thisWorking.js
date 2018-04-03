0d694d28bf20ab0bc2e9b327d21c40dcfe207648
akashkumar1.patel <akashkumar1.patel@ril.com>
2018-04-03 19:49:52 +0530

    comments on this

--- thisWorking.js
+++ thisWorking.js
@@ -12,13 +12,22 @@ var cat = {
     // printInfo is invoked with context of cat so it will have this bind to cat.
     printInfo:  function(){
         console.log("Name:", this.name, "Color:", this.color, "Age:", this.age); //prints correctly
-        // this function is invoked without any context so it will have this bind to window object
-        nestedFunction =  function(){
+       
+      /*   //As arrow function does not create their own "this". so by they will refer "this" of their encloser
+        nestedFunction =  ()=>  {
+            console.log(this);
+            console.log("Name:", this.name, "Color:", this.color, "Age:", this.age); //prints correctly
+        }
+        nestedFunction(); */
+
+
+        // this function is invoked without any context so it will have this bind to window object        
+        /* nestedFunction =  function(){
             console.log(this);
             console.log("Name:", this.name, "Color:", this.color, "Age:", this.age); //prints in correctly
         }
-        //nestedFunction.call(this);
-        nestedFunction();
+        nestedFunction(); 
+        nestedFunction.call(this);*/
     }
 }
 cat.printInfo()
